973ca2bedfeeec3fb183118cf7679da1a94f954c
mrousavy <marcrousavy@hotmail.com>
2017-04-04 15:25:19 +0200

    Added KeyUp/Down Methods and Client

--- HedgehogClient/ControlKeys.cs
+++ HedgehogClient/ControlKeys.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HedgehogClient {
-    internal class ControlKeys {
-        public enum MovementKeys {
-            W,          //Forward
-            A,          //Left
-            S,          //Back
-            D,          //Right
-            Plus,       //Speed Up
-            Minus,      //Speed Down
-            Space       //Hand Brake
+    internal static class ControlKeys {
+        public enum MovementKey {
+            W = 0,          //Forward
+            A = 1,          //Left
+            S = 2,          //Back
+            D = 3,          //Right
+            Plus = 4,       //Speed Up
+            Minus = 5,      //Speed Down
+            Space = 6,      //Hand Brake
+            Stop = 7        //Stop any Movement
+        }
+
+        public static MovementKey GetKey(Key? key) {
+            if(key == null)
+                return MovementKey.Stop;
+
+            switch(key) {
+                case Key.W:
+                    return MovementKey.W;
+                case Key.A:
+                    return MovementKey.A;
+                case Key.S:
+                    return MovementKey.S;
+                case Key.D:
+                    return MovementKey.D;
+                case Key.OemPlus:
+                case Key.Add:
+                    return MovementKey.Plus;
+                case Key.OemMinus:
+                case Key.Subtract:
+                    return MovementKey.Minus;
+                case Key.Space:
+                    return MovementKey.Space;
+                default:
+                    throw new Exception("Wrong Input");
+            }
         }
     }
 }
--- HedgehogClient/HedgehogClientWindow.xaml.cs
+++ HedgehogClient/HedgehogClientWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -13,8 +14,17 @@ namespace HedgehogClient {
         private IPAddress _address;
         private IPEndPoint _endPoint;
         private TcpClient _client;
+        private SocketStatus _status;
 
         private const int _port = 8000;
+
+
+        private enum SocketStatus {
+            Disconnected,
+            Connecting,
+            Connected,
+            Busy
+        }
 
 
         public HedgehogClientWindow(IPAddress address) {
@@ -25,6 +35,7 @@ namespace HedgehogClient {
         }
 
         private async void Connect() {
+            Cursor = Cursors.AppStarting;
             statusLabel.Content = "Connecting...";
             statusLabel.Foreground = Brushes.Orange;
 
@@ -32,34 +43,81 @@ namespace HedgehogClient {
             _client = new TcpClient();
 
             try {
-                Cursor = Cursors.AppStarting;
+                _status = SocketStatus.Connecting;
 
                 await _client.ConnectAsync(_address, _port);
 
                 if(_client.Connected) {
+                    _status = SocketStatus.Connected;
                     statusLabel.Content = "Connected";
                     statusLabel.Foreground = Brushes.Green;
                 } else {
+                    _status = SocketStatus.Disconnected;
                     statusLabel.Content = "Disconnected";
                     statusLabel.Foreground = Brushes.Red;
                 }
-
-
             } catch(Exception e) {
+                _status = SocketStatus.Disconnected;
                 statusLabel.Content = "Disconnected";
                 statusLabel.Foreground = Brushes.Red;
                 MessageBox.Show($"Could not Connect!\n\r{e.Message}", "Error Connecting to Hedgehog!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+
+            Cursor = Cursors.Arrow;
         }
 
+
+
+
+
+        private void Log(string message) {
+            logBox.Text += $"({DateTime.Now}) >" + message + Environment.NewLine;
+        }
+
+        //KeyDown Locks a Key (e.g. W) and drives forward till KeyUp)
         private void WindowKeyDown(object sender, KeyEventArgs e) {
-            Key key = e.Key;
+            try {
+                Key key = e.Key;
 
+                ControlKeys.MovementKey movementKey = ControlKeys.GetKey(key);
+
+                SendKey(movementKey);
+            } catch {
+                //Wrong input
+            }
+        }
+
+        //KeyUp Unlocks a Key (e.g. W)
+        private void WindowKeyUp(object sender, KeyEventArgs e) {
+            SendKey(ControlKeys.MovementKey.Stop);
+        }
+
+        private async void SendKey(ControlKeys.MovementKey movementKey) {
+            while(_status == SocketStatus.Busy) {
+                await Task.Delay(10);
+            }
+
+            if(_status == SocketStatus.Disconnected || _status == SocketStatus.Connecting) {
+                return;
+            }
+
+            byte key = (byte)movementKey;
+            byte[] message = { key };
+
+            _client.Client.BeginSend(message, 0, 1, SocketFlags.None, Sent, null);
         }
 
 
+        private void Sent(IAsyncResult result) {
+            if(result.IsCompleted) {
+                Console.WriteLine("Sent!");
+            } else {
+                Disconnected("Tried to send Message to Hedgehog, failed");
+            }
+        }
 
         private void Disconnected(string message = null) {
+            _status = SocketStatus.Disconnected;
             statusLabel.Content = "Disconnected";
             statusLabel.Foreground = Brushes.Red;
 
